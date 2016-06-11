885adcf0d916a50a20cb5ae0abf7339e2510341c
Timbuli Remus <kapucin28@gmail.com>
2016-06-11 14:28:18 +0300

    Adding variables

--- app/src/main/java/com/kapucin28/rollandroiddb/MainActivity.java
+++ app/src/main/java/com/kapucin28/rollandroiddb/MainActivity.java
@@ -1,13 +1,29 @@
 package com.kapucin28.rollandroiddb;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
+    // SQL variables & constants--------------------------------------------------------------------
+    private Cursor cursor;
+    private SQLiteDatabase sqLiteDatabase = null;
+    private final String databaseName = "SQL DB";
+    private final String tableName = "sql";
+    private int idColumn, nameColumn, emailColumn, phoneColumn;
+    private String name, email, phone, id, personDetails;
+    private File file;
+    //----------------------------------------------------------------------------------------------
+
+    // OnCreate method------------------------------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_layout);
     }
+    //----------------------------------------------------------------------------------------------
 }
