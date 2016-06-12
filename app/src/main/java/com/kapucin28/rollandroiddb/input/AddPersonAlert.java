1a5ceac127156032d684b22dd95a66644734fd7c
Timbuli Remus <kapucin28@gmail.com>
2016-06-12 21:30:08 +0300

    Updating classes

---
+++ app/src/main/java/com/kapucin28/rollandroiddb/input/AddPersonAlert.java
@@ -0,0 +1,19 @@
+package com.kapucin28.rollandroiddb.input;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by K@puc!n on 12-Jun-16.
+ *
+ */
+public class AddPersonAlert extends DialogFragment{
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
