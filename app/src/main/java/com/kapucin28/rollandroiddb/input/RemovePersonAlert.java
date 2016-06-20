59ad93d3cd992d083a879fb137bbb9ca01f8d434
Timbuli Remus <kapucin28@gmail.com>
2016-06-20 13:56:44 +0300

    Adding views

--- app/src/main/java/com/kapucin28/rollandroiddb/input/RemovePersonAlert.java
+++ app/src/main/java/com/kapucin28/rollandroiddb/input/RemovePersonAlert.java
@@ -4,6 +4,10 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by K@puc!n on 12-Jun-16.
@@ -11,9 +15,22 @@ import android.support.v4.app.DialogFragment;
  */
 public class RemovePersonAlert extends DialogFragment {
 
+    // SendResult interface-------------------------------------------------------------------------
+    private SendResult sendResult;
+    //----------------------------------------------------------------------------------------------
+
+    // Layout variables-----------------------------------------------------------------------------
+    private LayoutInflater layoutInflater;
+    private View view;
+    private EditText removeID;
+    private AlertDialog.Builder builder;
+    //----------------------------------------------------------------------------------------------
+
+    // OnCreate method------------------------------------------------------------------------------
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         return super.onCreateDialog(savedInstanceState);
     }
+    //----------------------------------------------------------------------------------------------
 }
