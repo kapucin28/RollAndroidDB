6d1f4a540916a07708d0a13ce1e814b2d043e7cb
Timbuli Remus <kapucin28@users.noreply.github.com>
2016-06-26 13:29:00 +0300

    Adding packages & implementations

--- app/src/main/java/com/kapucin28/rollandroiddb/main/MainActivity.java
+++ app/src/main/java/com/kapucin28/rollandroiddb/main/MainActivity.java
@@ -4,12 +4,27 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 
 import com.kapucin28.rollandroiddb.R;
+import com.qdv8.sqldbcreator.input.AddPersonAlert;
+import com.qdv8.sqldbcreator.input.RemovePersonAlert;
 
 import java.io.File;
+import java.util.Set;
+import java.util.TreeSet;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        AddPersonAlert.SendResults, RemovePersonAlert.SendResult{
 
     // SQL variables & constants--------------------------------------------------------------------
     private Cursor cursor;
